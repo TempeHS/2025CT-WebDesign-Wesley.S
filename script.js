c4382616da35998383dec4462f000ae86c92e409
WJSmith09 <Wesley.smith7@education.nsw.gov.au>
2024-03-11 14:13:55 +1100

    Start Laying Out Game

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt("Please enter your name");
+let playerGuess;
+let playerFeedback = "test";
+let computerGuess;
+loadDIV ();
 
-if (true) {
-    alert(myIntro + enterName);
+if (enterName =="Aidan Vu")
+    enterName = "Shortass"
+
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "<H1>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML +
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML +
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "<H1><H2>Guess a number from 1 to 10 please<H2>"
+}
+else { 
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName +"<H1><H2>Guess a number from 1 to 10 please<H2>"
 }
